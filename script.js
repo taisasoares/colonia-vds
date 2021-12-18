c0f632849ae6e08b77e1bdf3d4bfee12501be882
Taisa Soares <soares.taisa16@gmail.com>
2021-12-18 11:12:11 -0300

    feat: header finished

--- script.js
+++ script.js
@@ -16,3 +16,7 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+function inscrevase() {
+  alert("Esse botão manda para o formulário de inscrição");
+}
