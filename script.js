3e68ec9be3ef2f199cd33a3eb0a69c2d160f903c
Taisa Soares <soares.taisa16@gmail.com>
2021-12-20 08:59:58 -0300

    fix: fotos section

--- script.js
+++ script.js
@@ -20,20 +20,3 @@ function closeMenu() {
 function inscrevase() {
   alert("Esse botão manda para o formulário de inscrição");
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  var stream = document.querySelector(".gallery__stream");
-  var items = document.querySelectorAll(".gallery__item");
-
-  var prev = document.querySelector(".gallery__prev");
-  prev.addEventListener("click", function () {
-    stream.insertBefore(items[items.length - 1], items[0]);
-    items = document.querySelectorAll(".gallery__item");
-  });
-
-  var next = document.querySelector(".gallery__next");
-  next.addEventListener("click", function () {
-    stream.appendChild(items[0]);
-    items = document.querySelectorAll(".gallery__item");
-  });
-});
