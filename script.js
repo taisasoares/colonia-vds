b1435742b7ac96899de956051ff1839a1149086a
Taisa Soares <soares.taisa16@gmail.com>
2021-12-22 14:29:33 -0300

    refactor: class to id in sections

--- script.js
+++ script.js
@@ -20,3 +20,33 @@ function closeMenu() {
 function inscrevase() {
   alert("Esse botão manda para o formulário de inscrição");
 }
+
+// Test Zone
+
+// let slider = document.querySelector(".slide-container");
+// let sliderIndividual = document.querySelectorAll(".slide");
+// let contador = 1;
+// let width = sliderIndividual[0].clientWidth;
+// let intervalo = 3000;
+
+// window.addEventListener("resize", function () {
+//   width = sliderIndividual[0].clientWidth;
+// });
+
+// setInterval(function () {
+//   slides();
+// }, intervalo);
+
+// function slides() {
+//   slider.style.transform = "translate(" + -width * contador + "px)";
+//   slider.style.transition = "transform .8s";
+//   contador++;
+
+//   if (contador == sliderIndividual.length) {
+//     setTimeout(function () {
+//       slider.style.transform = "translate(0px)";
+//       slider.style.transition = "transform 0s";
+//       contador = 1;
+//     }, 1500);
+//   }
+// }
